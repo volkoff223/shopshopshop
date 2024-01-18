31e57129092fb0547e52f3f0949d18ef794b42b2
Nick Voelker <volkoff223@gmail.com>
2024-01-18 16:32:54 -0500

    Removed hero

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script>
+	export let data;
 </script>
 
-<div class="hero h-60">
-	<div class="hero-overlay bg-opacity-80 rounded-lg" />
-	<div class="hero-content text-center text-neutral-content">
+{#if !data.user}
+	<div class="container">
 		<div class="max-w-md">
 			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<a href="/register">
+				<button class="btn btn-primary">Get Started</button>
+			</a>
+		</div>
+	</div>
+{:else}
+	<div class="container">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Hello there {data.user.name}</h1>
 			<p class="mb-5">Click the get started button to update your profile.</p>
 			<a href="/my/settings/profile">
 				<button class="btn btn-primary">Get Started</button>
 			</a>
 		</div>
 	</div>
-</div>
+{/if}
